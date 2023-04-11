a407c938d31d70f379ba3fc5e57f02a2294c3982
jimanderss0n <117285803+jimanderss0n@users.noreply.github.com>
2023-04-11 18:24:48 +0200

    LocalStorage
    Everything is done except the complete task to local storage

--- task-interaction.js
+++ task-interaction.js
@@ -1,11 +1,24 @@
+const inputElement = document.getElementById("taskInput");
 const toDoListElement = document.getElementById("tasks");
+const button = document.getElementById("addTaskButton");
 let taskList = [];
 
-function clickHandler() {
-  const inputElement = document.getElementById("taskInput");
+let saveJSON;
+
+function saveToLocal() {
+  saveJSON = JSON.stringify(taskList);
+  localStorage.tasks = saveJSON;
+}
+
+if (localStorage.tasks) {
+  taskList = JSON.parse(localStorage.tasks);
+}
+
+button.addEventListener("click", () => {
   taskList.push(inputElement.value);
   taskHandler();
-}
+  saveToLocal();
+});
 
 function taskHandler() {
   toDoListElement.innerHTML = "";
@@ -16,40 +29,35 @@ function taskHandler() {
 
     const removeButtonElement = document.createElement("button");
     removeButtonElement.innerText = "Remove";
-    toDoListElement.appendChild(removeButtonElement);
+    taskElement.appendChild(removeButtonElement);
     removeButtonElement.classList.add("remove");
-
-    const doneButtonElement = document.createElement("button");
-    doneButtonElement.innerText = "Completed";
-    toDoListElement.appendChild(doneButtonElement);
-    doneButtonElement.classList.add("done");
 
     const index = taskList.indexOf(task);
     removeButtonElement.addEventListener("click", () => {
       taskList.splice(index, 1);
       taskHandler();
+
+      saveToLocal();
     });
+
+    const doneButtonElement = document.createElement("button");
+    doneButtonElement.innerText = "Completed";
+    taskElement.appendChild(doneButtonElement);
+    doneButtonElement.classList.add("done");
+
+    //ChatGPT Below
+    doneButtonElement.addEventListener("click", function () {
+      taskElement.style.textDecoration = "line-through";
+
+      // Save the line-through style to localStorage
+      localStorage.setItem("toDoListStyle", "line-through");
+    });
+
+    // Retrieve the line-through style from localStorage on page load
+    const toDoListStyle = localStorage.getItem("toDoListStyle");
+    if (toDoListStyle) {
+      taskElement.style.textDecoration = toDoListStyle;
+    }
   }
 }
-
-const inputElement = document.getElementById("taskInput");
-//const addTaskElement = document.getElementById("addTask");
-
-function loadHandler() {
-  const button = document.getElementById("addTaskButton");
-  button.addEventListener("click", clickHandler);
-
-  //button.onclick = addTask;
-}
-
-function addTask() {
-  taskInput.innerHTML = "";
-  if (taskList) {
-    taskInput.innerHTML = inputElement.value;
-  }
-}
-
-/*const inputValue = inputElement.value;
-toDoListElement.innerHTML = inputValue;*/
-
-window.addEventListener("load", loadHandler);
+taskHandler();
